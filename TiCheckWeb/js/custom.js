e397f859b111487b98752e7e2dcf4abb7b29026f
BillHHHuang <BillHuangg@gmail.com>
2014-03-06 00:04:04 +0800

    小的修改
    适应不同屏幕的背景图。

--- TiCheckWeb/js/custom.js
+++ TiCheckWeb/js/custom.js
@@ -1,6 +1,22 @@
 //  
 jQuery(document).ready(function ($) {
 	
+	$(window).load(resetBannerBG);
+
+	$(window).resize(resetBannerBG);          
+    function resetBannerBG()
+    {
+
 	//
+		var rate=2560/1993;
+		var width=$(window).width();
+		var height=rate*width;
+		$(".BannnerBG_Box").css("height",height);
+
+		var marginTop=-($(".BannerElementBox").height()+$(".Header").height());
+		$(".BannnerBG_Box").css("margin-top",marginTop);
+
+		$(".Main_IntroductionBox").css("margin-top",-height*0.4);
+    }
  
 });
