c743978024bae584063f5ef2ab4d0f89a198d264
BillHHHuang <BillHuangg@gmail.com>
2014-03-06 21:11:43 +0800

    小修改
    字体对chrome ie不兼容，布局在宽度小于1024时会有所改变

--- TiCheckWeb/js/custom.js
+++ TiCheckWeb/js/custom.js
@@ -1,37 +1,45 @@
 //  
 jQuery(document).ready(function ($) {
 	
+	
 	$(window).load(ResetBannerBGBox);
 
 	$(window).resize(ResetBannerBGBox);  
 
-
-
+	
 	function ResetBannerBGBox()
 	{
 		var minDafultWidth=1280;
 		var rate=2560/1558;
 		var width=$(window).width();
 		
+		//
 		if(width>minDafultWidth)
 		{
+			
+			//手动设置背景图大小
 			$(".BannnerBG_Box").css("width",width);
+			$(".Footer").css("width",width);
 		}
 		else if(width>1024)
 		{
 			
+			//设置背景图宽度高度与footer宽度
 			$(".BannnerBG_Box").css("width",width);
-			
 			var height=width/rate;
 			$(".BannnerBG_Box").css("height",height);
+			
+			$(".Footer").css("width",width);
 		}
 		else
 		{
 			
+			//当小到出现滚动条后,即强制大小
 			$(".BannnerBG_Box").css("width",1024);
-			
 			var height=width/rate;
 			$(".BannnerBG_Box").css("height",height);
+			
+			$(".Footer").css("width",1024);
 		}
 		
 		
@@ -46,72 +54,14 @@ jQuery(document).ready(function ($) {
  		var offset=height/3;
  		var rate=338/814;
  		
+ 		//设置phoneview大小 
  		var imageHeight=offset/(rate*0.9);
  		$("#PhoneViewBox img").css("height",imageHeight);
  		
+ 		//设置位置
  		$("#PhoneViewBox").css("margin-top",offset);
  		$("#leftPanelElementBox ").css("margin-top",offset);
  		
  	}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    function resetBannerBG2()
-    {
-
-	//
-		var rate=2560/1993;
-		var width=$(window).width();
-		if(width<=1280)
-		{
-			width=1280;
-			$(".BannnerBG_Box").css("width",width);
-		}
-			//
-			var height=rate*width;
-			$(".BannnerBG_Box").css("height",height);
-
-			var offset=resetBannerElement(height);
-			
-			//
-			//var marginTop=-($(".BannerElementBox").height()+$(".Header").height()+offset);
-			//$(".BannnerBG_Box").css("margin-top",marginTop);
-
-			//
-			$(".Main_IntroductionBox").css("margin-top",-height*0.4);
-
-
-
-		/*}
-		else
-		{
-			var height=rate*width;
-			$(".BannnerBG_Box").css("height",height);
-
-			var marginTop=-($(".BannerElementBox").height()+$(".Header").height());
-			$(".BannnerBG_Box").css("margin-top",marginTop);
-
-			$(".Main_IntroductionBox").css("margin-top",-height*0.4);
-		}*/
-    }
-
-
- 	
 });
